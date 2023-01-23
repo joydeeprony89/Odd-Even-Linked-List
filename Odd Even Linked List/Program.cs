aba23ebd4128d612911189ec6923d325d459ca16
jd_786 <jaydeep_shil@certree.com>
2023-01-23 17:12:19 +0530

    Add project files.

---
+++ Odd Even Linked List/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);  
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+head.next.next.next.next.next = new ListNode(6);
+Solution s = new Solution();
+var answer = s.OddEvenList(head);
+while (answer != null)
+{
+  Console.WriteLine(answer.val);
+  answer = answer.next;
+}
+
+public class ListNode
+{
+  public int val;
+  public ListNode next;
+  public ListNode(int val = 0, ListNode next = null)
+  {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+public class Solution
+{
+  public ListNode OddEvenList(ListNode head)
+  {
+    if (head == null) return head;
+    var odd = head;
+    var proxy1 = odd;
+    var even = head.next;
+    var proxy2 = even;
+    while (even != null && even.next != null && odd != null && odd.next != null)
+    {
+      odd.next = even.next;
+      odd = odd.next;
+      even.next = odd.next;
+      even= even.next;  
+    }
+    odd.next = proxy2;
+    return proxy1;
+  }
+}
